b6b4f6c94d9d0b814325a166296499d576a97c03
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-11-24 17:08:34 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
 """
-A function that determine the fewest number of coins
-needed to meet a given amount total.
+Coin Change Algorithm
 """
 
 
 def makeChange(coins, total):
-    """
-    Calculate the min num of coins needed to make change for a given amount.
-
+    """Calculate the fewest number needed to meet,
+    needed to meet a given total amount.
     Args:
-        coins (list): List of coin denominations available.
-        total (int): The total amount for which change needs to be made.
-
-    Returns:
-        int: Min num of coins needed. Returns -1 if change cannot be made.
+        coins ([list]): A list of coin values available.
+        total ([number]): The target amount
+    Return: The fewest number of coins needed to reach the total,
+    or -1 if not possible.
     """
     if total <= 0:
         return 0
 
-    remaining_amount = total
-    coins_count = 0
-    coin_index = 0
-    sorted_coins = sorted(coins, reverse=True)
-    num_coins = len(coins)
+    coins.sort(reverse=True)
 
-    while remaining_amount > 0:
-        if coin_index >= num_coins:
-            return -1
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
 
-        if remaining_amount - sorted_coins[coin_index] >= 0:
-            remaining_amount -= sorted_coins[coin_index]
-            coins_count += 1
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
         else:
-            coin_index += 1
+            i += 1
 
-    return coins_count
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
